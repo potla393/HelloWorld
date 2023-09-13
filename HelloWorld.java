dcabe9aaec60fcbd4a164b98f79ebb3210488b82
potla393 <79921095+potla393@users.noreply.github.com>
2023-09-13 19:02:22 +0530

    Update HelloWorld.javaret

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-     System.out.println("Hello world!5678");
+     System.out.println("Hello world!567777777777777778");
   }
 }
